6244850e48a47a5ec923cbad4e2772dd88837a6f
Jhon Esparragoza <jhonantonyesp@gmail.com>
2023-12-05 16:33:51 +0100

    subida de proyecto

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text("Proyecto Flutter"),
+        ),
+      ),
+    );
+  }
+}
