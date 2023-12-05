bb1d51b67e2e5d57028bd38a5cac3bc8422a5cef
Jhon Esparragoza <jhonantonyesp@gmail.com>
2023-12-05 17:52:03 +0100

    Cambios

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
-  const myApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +14,36 @@ class myApp extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.blue,
           title: const Text("Proyecto Flutter"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blueAccent,
+          child: Icon(Icons.add),
+          onPressed: () {
+            print("Presionado");
+          },
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.business), label: "Negocio"),
+            BottomNavigationBarItem(icon: Icon(Icons.school), label: "Escuela"),
+          ],
+        ),
+        drawer: Drawer(
+          backgroundColor: Colors.blue,
+          child: ListView(
+            padding: const EdgeInsets.all(16.0),
+            // ignore: prefer_const_literals_to_create_immutables
+            children: [
+              const ListTile(
+                  leading: Icon(Icons.message), title: Text("Mensajes")),
+              const ListTile(
+                  leading: Icon(Icons.settings), title: Text("Ajustes")),
+              const ListTile(
+                  leading: Icon(Icons.exit_to_app), title: Text("Salir")),
+            ],
+          ),
         ),
       ),
     );
